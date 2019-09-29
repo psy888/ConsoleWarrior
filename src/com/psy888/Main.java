64f802fc267933dfe940a7f98f8474a6171a18ce
psy888 <tbilisi.zp.snab@gmail.com>
2019-09-29 11:59:33 +0300

    fix attack, add info flow

--- src/com/psy888/Main.java
+++ src/com/psy888/Main.java
@@ -4,11 +4,12 @@ import java.util.Scanner;
 
 public class Main {
 
-    int currentMove; //текущий ход 0 или 1
+    static int currentMove; //текущий ход 0 или 1
 
     public static void main(String[] args) {
 	// write your code here
 
+        startGame();
     }
 
     public void makeMove(){
@@ -16,9 +17,9 @@ public class Main {
 
     }
 
-    public void whoIsNext(){
+    public static void whoIsNext(){
 
-        this.currentMove = (int) (Math.random() * 2);
+        currentMove = (int) (Math.random() * 2);
     }
 
     /*
@@ -27,7 +28,7 @@ public class Main {
         2 - heal
         ( Math.random() * (b-a) ) + a
      */
-    public int attack(boolean lessThan35){
+    public static int attack(boolean lessThan35){
         int hp = 0;
 
         int chance = (int) (Math.random() * 3);
@@ -35,8 +36,8 @@ public class Main {
         if (lessThan35){
             // add 35 % of luck
         }
-        int min;
-        int max;
+        int min = 0;
+        int max = 0;
         boolean sign = true; //true -> " - " ; false -> " + "
         switch (chance)
         {
@@ -52,12 +53,15 @@ public class Main {
                 break;
         }
 
+        hp = (int)(Math.random() * (max-min)  + min);
         //todo процентовка исходов
+
+        hp = (sign)?hp*-1:hp; //heal or damage
 
         return hp;
     }
 
-    public void startGame(){
+    public static void startGame(){
 
         //инициализация игрока Пользователя
         Scanner scanner = new Scanner(System.in);
@@ -69,20 +73,46 @@ public class Main {
         Person pc = new Person("Computer");
 
 
+        Person[] player = new Person[]{user,pc};
+
         System.out.println("Игра началась!");
         //todo вставить текстовые картинки
         do {
             //game process here..
             System.out.println("Следующий ход игрока :");
             whoIsNext(); // определить за кем следующий ход
-            System.out.println((currentMove>0)?pc.getName():user.getName()); //1 - pc , 0 - user
-            System.out.println("\n---------------------------------------------------------------------------");
+            System.out.println(player[currentMove].getName()); //1 - pc , 0 - user
+            System.out.println("***************************************************************************************");
+            System.out.println("Игрок " +  player[currentMove].getName() + " атакует!");
+            System.out.println("***************************************************************************************");
+
+            int attackResult = 0;
+            //attack
             if (currentMove>0) //pc
             {
-
-                user.setHP(user.getHP() - attack(pc.getHP()<35));
+                attackResult = attack(false);
+                player[0].setHP(player[0].getHP() + attackResult);
             }else{ // user
-                pc.setHP(pc.getHP() - attack(false));
+                attackResult = attack(player[1].getHP()<35);
+                player[1].setHP(player[1].getHP() + attackResult);
+            }
+            //results
+            if(attackResult>0){
+                System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+                System.out.println("Игрок " +  player[currentMove].getName() + " промахивается и розливает исцеляющее зелье на противника\n" +
+                        "Противник получает дополнительно +" + attackResult + " очков здоровья.");
+                System.out.println("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++");
+            }else{
+                System.out.println("---------------------------------------------------------------------------------------");
+                System.out.println("Игрок " +  player[currentMove].getName() + " наносит удар и отнимает у противника " + attackResult +
+                        " очков здоровья.");
+                System.out.println("---------------------------------------------------------------------------------------");
+            }
+
+            try {
+                Thread.sleep(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
 
         }while (user.getHP()>0&&pc.getHP()>0);
